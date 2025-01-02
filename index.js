2dfe94fa9ca411643a10b30eebe79450c8ce1b1a
SujalNigam <sujalnigam2003@gmail.com>
2025-01-02 19:22:15 +0530

    changes

--- index.js
+++ index.js
@@ -37,7 +37,8 @@ const server = app.listen(port, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "https://frolicking-halva-484bbc.netlify.app/",
     credentials: true,
   },
 });
